2d594233fcb158ca207dfb50cd9f794537def9bd
BrandonPessman <pessman.brandon@gmail.com>
2019-10-19 01:11:37 -0500

    Updates socket.io connections

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import openSocket from 'socket.io-client'
-const socket = openSocket('http://localhost:8000')
+const socket = openSocket('http://localhost:8001')
 
 export default function Dashboard() {
-  return <div></div>
+  const [players, setPlayers] = useState([])
+
+  socket.emit('init')
+
+  socket.on('load', res => {
+    var newPlayers = []
+    for (let i = 0; i < res.length; i++) {
+      newPlayers[i] = (
+        <p>
+          First: {res[i].first} Last: {res[i].last} Wins: {res[i].wins} Losses:{' '}
+          {res[i].losses}
+        </p>
+      )
+    }
+
+    setPlayers(newPlayers)
+  })
+
+  return (
+    <div>
+      <h1>Ping Pong</h1>
+      {players}
+    </div>
+  )
 }
