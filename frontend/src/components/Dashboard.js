7f6503f34b923840a77ab9d292aedda4c05d0804
BrandonPessman <pessman.brandon@gmail.com>
2019-10-21 03:05:19 -0500

    Fixes Socket.io refresh issues

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,30 +1,73 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import openSocket from 'socket.io-client'
 const socket = openSocket('http://localhost:8001')
 
-export default function Dashboard() {
-  const [players, setPlayers] = useState([])
+export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
 
-  socket.emit('init')
-
-  socket.on('load', res => {
-    var newPlayers = []
-    for (let i = 0; i < res.length; i++) {
-      newPlayers[i] = (
-        <p>
-          First: {res[i].first} Last: {res[i].last} Wins: {res[i].wins} Losses:{' '}
-          {res[i].losses}
-        </p>
-      )
+    this.state = {
+      players: '',
     }
 
-    setPlayers(newPlayers)
-  })
+    this.handlePlayerUpdate = this.handlePlayerUpdate.bind(this)
+  }
 
-  return (
-    <div>
-      <h1>Ping Pong</h1>
-      {players}
-    </div>
-  )
+  componentDidMount() {
+    socket.emit('init')
+
+    socket.on('load', res => {
+      var newPlayers = []
+      for (let i = 0; i < res.length; i++) {
+        newPlayers[i] = (
+          <div key={i}>
+            <h2>
+              {res[i].first} {res[i].last}
+            </h2>
+            <h3>Record</h3>
+            <button id={'+w' + res[i].last} onClick={this.handlePlayerUpdate}>
+              +
+            </button>{' '}
+            <button id={'-w' + res[i].last} onClick={this.handlePlayerUpdate}>
+              -
+            </button>{' '}
+            {res[i].wins} - {res[i].losses}{' '}
+            <button id={'+l' + res[i].last} onClick={this.handlePlayerUpdate}>
+              +
+            </button>{' '}
+            <button id={'-l' + res[i].last} onClick={this.handlePlayerUpdate}>
+              -
+            </button>
+            <h3>Ratio</h3>
+            <p>Ratio: {res[i].wins / res[i].losses}</p>
+            <br />
+          </div>
+        )
+      }
+
+      this.setState({ players: newPlayers })
+    })
+  }
+
+  handlePlayerUpdate(event) {
+    var elementId = event.target.id
+
+    var payload = {
+      operator: elementId[0],
+      type: elementId[1],
+      id: elementId.slice(2, elementId.length),
+    }
+
+    socket.emit('update player', payload)
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h1>Ping Pong Tracker</h1>
+        <hr></hr>
+        {this.state.players}
+      </div>
+    )
+  }
 }
